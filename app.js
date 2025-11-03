b1b05003c30e8f683178cb8d20add5719275ed0b
ImVolcano <maks_kasimov@internet.ru>
2025-11-03 20:13:30 +0300

    Update run_tests

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ http.createServer(function(request,response){
      
 }).listen(3000, "127.0.0.1",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+    setTimeout(() => {
+  console.log('Выход через 5 секунд');
+  process.exit(0); // Выход с кодом успеха
+}, 5000);;
+})
